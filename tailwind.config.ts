b90b576ff33bbe3eceb7d371d0fa8bc6b4ee6153
ankitku3101 <ankit.ku3101@gmail.com>
2024-11-17 02:09:40 +0530

    Fixed border radius config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,11 +37,6 @@ const config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
